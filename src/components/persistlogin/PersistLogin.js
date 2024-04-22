8e508a19dab518adb25ace4abd7aafa587678907
Killian0812 <93117684+Killian0812@users.noreply.github.com>
2024-04-22 19:13:18 +0700

    feat: GetStream API

--- src/components/persistlogin/PersistLogin.js
+++ src/components/persistlogin/PersistLogin.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import useRefreshToken from "../../hooks/useRefreshToken";
 import useAuth from "../../hooks/useAuth";
+import Loading from "../Loading";
 
 const PersistLogin = () => {
     const [isLoading, setIsloading] = useState(true);
@@ -38,7 +39,7 @@ const PersistLogin = () => {
             so if the device is not trusted => go straight to Authenticate component,
             if refresh => clear auth => authenticate fail => navigate back to login */}
             {!trusted ? <Outlet></Outlet>
-                : isLoading ? <p>Loading...</p>
+                : isLoading ? <Loading />
                     : <Outlet></Outlet>
             }
         </>
