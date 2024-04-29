a1dca34c2ace581d0b59f9fc42ee6202c6bd7638
Michael Cao <caomanhcuong1609@gmail.com>
2024-04-29 21:20:24 +0700

    ui: profile, settings

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 
 import PersistLogin from "./components/persistlogin/PersistLogin.js";
-import Authenticate from './components/authenticate/Authenticate.js';
+import Authenticate from "./components/authenticate/Authenticate.js";
 import NotFound from "./pages/home/NotFound.js";
 import Settings from "./pages/home/Settings.js";
 import Friends from "./pages/home/Friends.js";
@@ -13,7 +13,6 @@ import Layout from "./components/Layout.js";
 import Login from "./pages/auth/Login.js";
 import Register from "./pages/auth/Register.js";
 export default function App() {
-
   function handleCallbackResponse(response) {
     const GoogleJWT = response.credential;
     console.log(jwtDecode(GoogleJWT));
@@ -22,37 +21,44 @@ export default function App() {
   useEffect(() => {
     /* global google */
     google.accounts.id.initialize({
-      client_id: '118692739109-em2kp06md5s62ee8533ugpq3usq5e684.apps.googleusercontent.com',
-      callback: handleCallbackResponse
+      client_id:
+        "118692739109-em2kp06md5s62ee8533ugpq3usq5e684.apps.googleusercontent.com",
+      callback: handleCallbackResponse,
     });
   }, []);
 
   return (
-
     <Routes>
-
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/*" element={<NotFound />} />
 
       {/* persist user login */}
       <Route element={<PersistLogin />}>
-
         {/* require authentication  */}
         <Route element={<Authenticate />}>
-
           <Route path="/" element={<Layout />}>
             {/* Nested routes */}
-            <Route path="/profile" element={<Profile />} />
+
+            <Route
+              path="/profile"
+              element={
+                <Profile
+                  info={{
+                    icon: "https://media.gettyimages.com/id/545945904/photo/topshot-portugals-forward-cristiano-ronaldo-reacts-as-portugal-beats-france-1-0-to-clinch-the.jpg?s=612x612&w=0&k=20&c=I3NgDyB0NLaqZbWchp08_jRq7Ozbyc426bZ1hWLk6vE=",
+                    name: "Penaldu",
+                    address: "Korea",
+                  }}
+                />
+              }
+            />
+
             <Route path="/friends" element={<Friends />} />
             <Route path="/settings" element={<Settings />} />
             <Route path="/groups" element={<Groups />} />
           </Route>
-
         </Route>
-
       </Route>
-
     </Routes>
   );
 }
